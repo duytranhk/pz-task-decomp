2b201cade9ed6fe48dc00fd566663d30b0eeec9e
Duy Tran <duy.tran@patientzero.com.au>
2021-06-01 15:42:19 +1000

    update

---
+++ src/app.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement, FC } from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { Container } from '@material-ui/core';
+import Header from './components/header.component';
+import MenuBar from './components/menu-bar.component';
+import AzureDevopsProvider from './contexts/azure-devops.context';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#0097a7',
+        },
+        secondary: {
+            main: '#00838f',
+        },
+    },
+});
+
+const App: FC<any> = (): ReactElement => {
+    return (
+        <ThemeProvider theme={theme}>
+            <AzureDevopsProvider>
+                <MenuBar />
+                <Container maxWidth="lg">
+                    <Header />
+                </Container>
+            </AzureDevopsProvider>
+        </ThemeProvider>
+    );
+};
+
+export default App;
